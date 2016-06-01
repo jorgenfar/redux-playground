df5d85259426cb7aed4343a7207dab6f212b662b
Jørgen Faret <jorgen.faret@netlight.com>
2016-06-01 20:58:19 +0200

    Formatting

--- redux/sagas.js
+++ redux/sagas.js
@@ -2,11 +2,7 @@ import { takeEvery, delay } from 'redux-saga';
 import { put } from 'redux-saga/effects';
 
 import { SLIDE_MODES } from '../constants';
-import {
-  START,
-  nextSlide,
-  randomSlide,
-} from './actions';
+import { START, nextSlide, randomSlide } from './actions';
 
 function* slideShow(getState) {
   while (getState().playing) {
